3e3f95436f20ded16f8526ab4638f6fd5cd709c9
Tristan Hampton <tristanhampton@icloud.cmo>
2020-04-26 12:53:18 -0600

    Updates

--- footer.php
+++ footer.php
@@ -1,3 +1,20 @@
+    <footer>
+        <div class="bezel"></div>
+        <h2>Find Us On</h2>
+        <ul>
+            <li><a href="https://www.instagram.com/allcyclesyeg/">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3333 3333" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd"><path d="M1667 0c920 0 1667 746 1667 1667 0 920-746 1667-1667 1667C747 3334 0 2588 0 1667 0 747 746 0 1667 0zm-390 752h780c293 0 532 237 532 525v778c0 289-239 525-532 525h-780c-293 0-532-236-532-525v-778c0-289 240-525 532-525zm385 421c285 0 516 231 516 516s-231 516-516 516-516-231-516-516 231-516 516-516zm0 174c188 0 341 153 341 341s-153 341-341 341c-189 0-341-153-341-341s153-341 341-341zm499-246c46 0 84 37 84 84 0 46-37 84-84 84-46 0-84-37-84-84 0-46 37-84 84-84zm-820-200h652c245 0 445 199 445 443v656c0 244-200 443-445 443h-652c-245 0-445-199-445-443v-656c0-244 200-443 445-443z"/></svg>
+            </a></li>
+            <li><a href="https://www.facebook.com/AllCyclesYeg/">
+                <svg viewBox="0 0 3333 3333" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd"><path d="M1667 0c920 0 1667 746 1667 1667 0 920-746 1667-1667 1667C747 3334 0 2588 0 1667 0 747 746 0 1667 0zm186 1117h311V744h-311c-240 0-435 195-435 435v186h-249v373h249v994h373v-994h311l62-373h-373v-186c0-34 28-62 62-62z"/></svg>
+            </a></li>
+            <li><a href="https://www.youtube.com/channel/UCG51WlYJwBw7J_eyizmprag">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3333 3333" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd"><path d="M1667 0c920 0 1667 746 1667 1667 0 920-746 1667-1667 1667C747 3334 0 2588 0 1667 0 747 746 0 1667 0zm913 1294s-18-129-74-185c-71-74-151-75-187-79-261-19-652-19-652-19h-1s-392 0-652 19c-36 4-116 5-187 79-56 56-74 185-74 185s-19 151-19 302v141c0 151 19 302 19 302s18 129 74 185c71 74 164 72 206 80 149 14 634 19 634 19s392-1 653-19c36-4 116-5 187-79 56-56 74-185 74-185s19-151 19-302v-141c0-151-19-302-19-302zm-1107 615v-524l504 263-504 261z"/></svg>
+            </a></li>
+        </ul>
+    </footer>
+   
+   
     <?php  wp_footer(); ?>
 </body>
 </html>
--- front-page.php
+++ front-page.php
@@ -1,7 +1,26 @@
 <?php get_header() ?>
 
 <main class="front-page">
-    <h2>FRONT PAGE</h2>
+    <h1>Welcome to <span>All Cycles</span></h1>
+    <p>Our goal is to provide menstrual products to the homeless and income insecure, as these products are rarely donated to shelters.</p>
+    <p>We also strive to destigmatize menstruation and dissociate it from an inherently feminine experience.</p>
+    <div class="iframe-container">
+        <iframe src="https://www.youtube.com/embed/oadPgpei0j8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    <div class="products">
+        <p>Products Donated</p>
+        <p>13,314</p>
+    </div>
+
+    <div class="photo">
+        <p>Thomas Dang Donation</p>
+    </div>
+
+    <form action="#">
+        <h2>Join Our Mailing List</h2>
+        <p>Never miss an update</p>
+        <input type="text">
+    </form>
 </main>
 
 <?php get_footer() ?>
